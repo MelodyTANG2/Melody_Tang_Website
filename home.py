acd45c7f6295e9bd00b7641719f4c7b4366ac086
Melody_Tang <1155215047@link.cuhk.edu.hk>
2025-06-08 02:50:52 +0800

    home.py

--- home.py
+++ home.py
@@ -33,7 +33,7 @@ def home_page():
             """,
             unsafe_allow_html=True
         )
-        right_col.image(image, output_format='PNG', clamp=True, use_container_width=False, width=250)
+        right_col.image(image, output_format='PNG', clamp=True, use_container_width=False)
     else:
         right_col.warning("Profile image not found")
 
